63311d349e99fee6d7959125257910a4b28424be
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-07-25 09:53:44 +0545

    Understanding JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+let name = "Anish Joshi";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello {name} </h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, modi nam, omnis recusandae reiciendis cupiditate molestiae ipsa vel aut similique fuga rerum consequatur possimus! Sequi accusamus a quam quis? Iusto vitae quaerat accusantium distinctio.</p>
+      </div>
+    </>);
 }
 
 export default App;
