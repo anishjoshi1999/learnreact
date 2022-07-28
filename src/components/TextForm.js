f0ca9243cfa58a22bc1d110903b73efa771db7d0
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-07-28 09:57:27 +0545

    resolve wordCount function issue and added Covert to Titlecase feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,9 +12,28 @@ export default function TextForm(props) {
     let newText = text.toLowerCase()
     setText(newText)
   }
+  const handleTitleClick = () => {
+    // console.log("Uppercase function was clicked " + text)
+    const words = text.trim().split(" ")
+    const newwords = []
+    for(let word of words){
+        newwords.push(word[0].toUpperCase() + word.slice(1,))
+    }
+    
+    setText(newwords.join(" "))
+  }
+
   const handleOnChange = (event) => {
     // console.log("handleOnChange function was clicked")
     setText(event.target.value)
+  }
+  const wordCount = () => {
+    let result = text.trim().split(" ").includes('')
+    if(result !== true){
+      return text.split(" ").length
+    }else {
+      return 0
+    }
   }
   const [text, setText] = useState("");
   // text = "new text" - Wrong way to change the state
@@ -25,13 +44,14 @@ export default function TextForm(props) {
       <div className="container">
         <h2>{props.heading}</h2>
         <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} placeholder="Enter a text here"></textarea>
-        <button className='btn btn-primary mx-1' onClick={handleUpClick}>Covert to Uppercase</button>
-        <button className='btn btn-danger mx-1' onClick={handleLoClick}>Covert to Lowercase</button>
+        <button className='btn btn-primary m-1' onClick={handleUpClick}>Covert to Uppercase</button>
+        <button className='btn btn-success m-1' onClick={handleLoClick}>Covert to Lowercase</button>
+        <button className='btn btn-secondary m-1' onClick={handleTitleClick}>Covert to Titlecase</button>
 
       </div>
       <div className="container my-4">
         <h1>Your text Summary</h1>
-        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>{wordCount()} words, {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text}</p>
