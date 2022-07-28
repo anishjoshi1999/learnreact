f1627de4156eafe23b90107723f4fba5a25ef3b4
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-07-28 10:29:33 +0545

    Added Clear Text Functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,10 +16,10 @@ export default function TextForm(props) {
     // console.log("Uppercase function was clicked " + text)
     const words = text.trim().split(" ")
     const newwords = []
-    for(let word of words){
-        newwords.push(word[0].toUpperCase() + word.slice(1,))
+    for (let word of words) {
+      newwords.push(word[0].toUpperCase() + word.slice(1,))
     }
-    
+
     setText(newwords.join(" "))
   }
 
@@ -29,12 +29,16 @@ export default function TextForm(props) {
   }
   const wordCount = () => {
     let result = text.trim().split(" ").includes('')
-    if(result !== true){
+    if (result !== true) {
       return text.split(" ").length
-    }else {
+    } else {
       return 0
     }
   }
+  const handleClearClick = () => {
+    setText("")
+  }
+
   const [text, setText] = useState("");
   // text = "new text" - Wrong way to change the state
   // Correct way to change the state
@@ -44,15 +48,16 @@ export default function TextForm(props) {
       <div className="container">
         <h2>{props.heading}</h2>
         <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} placeholder="Enter a text here"></textarea>
-        <button className='btn btn-primary m-1' onClick={handleUpClick}>Covert to Uppercase</button>
-        <button className='btn btn-success m-1' onClick={handleLoClick}>Covert to Lowercase</button>
-        <button className='btn btn-secondary m-1' onClick={handleTitleClick}>Covert to Titlecase</button>
+        <button className='btn btn-primary m-2' onClick={handleUpClick}>Covert to Uppercase</button>
+        <button className='btn btn-success m-2' onClick={handleLoClick}>Covert to Lowercase</button>
+        <button className='btn btn-warning m-2' onClick={handleTitleClick}>Covert to Titlecase</button>
+        <button className='btn btn-danger m-2' onClick={handleClearClick}>Clear Text</button>
 
       </div>
       <div className="container my-4">
         <h1>Your text Summary</h1>
         <p>{wordCount()} words, {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <p>You need {text.split(" ").length * 0.008} minutes to read</p>
         <h2>Preview</h2>
         <p>{text}</p>
       </div>
