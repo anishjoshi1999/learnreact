54bc7eaac387830dd86d6febe6b96c09340b424a
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-07-29 06:37:39 +0545

    Added Copy Text Feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -32,6 +32,11 @@ export default function TextForm(props) {
       return 0
     }
   }
+  const handleCopy = () => {
+    let text = document.getElementById("myBox")
+    text.select()
+    navigator.clipboard.writeText(text.value)
+  }
   const handleClearClick = () => {
     setText("")
   }
@@ -54,6 +59,7 @@ export default function TextForm(props) {
         <button className='btn btn-success m-2' onClick={handleLoClick}>Covert to Lowercase</button>
         <button className='btn btn-warning m-2' onClick={handleTitleClick}>Covert to Titlecase</button>
         <button className='btn btn-danger m-2' onClick={handleClearClick}>Clear Text</button>
+        <button className='btn btn-info m-2' onClick={handleCopy}>Copy Text</button>
 
       </div>
       <div className="container my-4">
