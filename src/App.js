1044f1f7456f44ee3f8cba967e90ef4f4e73e403
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-07-30 11:13:55 +0545

    Improving Dark Mode & Refactoring App Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react'
+
 // import About from './components/About';
 function App() {
+  // whether dark mode is enable or not
+  const [mode,setMode] = useState("light")
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey'
+    }else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below"></TextForm>
+        <TextForm heading="Enter the text to analyze below"  mode={mode}></TextForm>
          {/* <About/> */}
       </div>
     </>);
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,7 +23,7 @@ export default function TextForm(props) {
     setText(newwords.join(" "))
   }
 
-  
+
   const wordCount = () => {
     let result = text.trim().split(" ").includes('')
     if (result !== true) {
@@ -52,23 +52,26 @@ export default function TextForm(props) {
   // setText("new text")
   return (
     <>
-      <div className="container">
-        <h2>{props.heading}</h2>
-        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} placeholder="Enter a text here"></textarea>
-        <button className='btn btn-primary m-2' onClick={handleUpClick}>Covert to Uppercase</button>
-        <button className='btn btn-success m-2' onClick={handleLoClick}>Covert to Lowercase</button>
-        <button className='btn btn-warning m-2' onClick={handleTitleClick}>Covert to Titlecase</button>
-        <button className='btn btn-danger m-2' onClick={handleClearClick}>Clear Text</button>
-        <button className='btn btn-info m-2' onClick={handleCopy}>Copy Text</button>
+      <div style={{ color: props.mode === "dark" ? "white" : "black" }}>
+        <div className="container" >
+          <h2>{props.heading}</h2>
+          <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === "dark" ? "grey" : "white" ,color: props.mode === "dark" ? "white" : "black"}} placeholder="Enter a text here"></textarea>
+          <button className='btn btn-primary m-2' onClick={handleUpClick}>Covert to Uppercase</button>
+          <button className='btn btn-success m-2' onClick={handleLoClick}>Covert to Lowercase</button>
+          <button className='btn btn-warning m-2' onClick={handleTitleClick}>Covert to Titlecase</button>
+          <button className='btn btn-danger m-2' onClick={handleClearClick}>Clear Text</button>
+          <button className='btn btn-info m-2' onClick={handleCopy}>Copy Text</button>
 
+        </div>
+        <div className="container my-4">
+          <h1>Your text Summary</h1>
+          <p>{wordCount()} words, {text.length} characters</p>
+          <p>You need {text.split(" ").length * 0.008} minutes to read</p>
+          <h2>Preview</h2>
+          <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+        </div>
       </div>
-      <div className="container my-4">
-        <h1>Your text Summary</h1>
-        <p>{wordCount()} words, {text.length} characters</p>
-        <p>You need {text.split(" ").length * 0.008} minutes to read</p>
-        <h2>Preview</h2>
-        <p>{text}</p>
-      </div>
+
     </>
 
   )
